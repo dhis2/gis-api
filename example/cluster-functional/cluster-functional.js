e1c67813e878c8016d74d6ebbabb21fc7cd1b7a9
Mark Polak <mark@thedutchies.com>
2015-11-17 21:41:10 +0100

    Re-add layer control and base maps to the example

--- example/cluster-functional/cluster-functional.js
+++ example/cluster-functional/cluster-functional.js
@@ -1,19 +1,46 @@
 import L from '../../src';
 
-L.Icon.Default.imagePath = '/images';
+function map() {
+    if (!map.map) {
+        L.Icon.Default.imagePath = '/images';
+        map.map = L.map('map').setView([8.577168, -11.871005], 8);
+    }
 
-var map = L.map('map').setView([8.577168, -11.871005], 8);
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+    return map.map;
+}
 
-fetch('/data/sierraleone.json')
-    .then(response => response.json())
-    .then(addCluster)
-    .catch(ex => console.log('parsing failed', ex));
+function getMapQuest() {
+    return L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+        subdomains: '1234',
+        attribution: '&copy; <a href="http://www.openstreetmap.org/">OpenStreetMap</a> and contributors, under an <a href="http://www.openstreetmap.org/copyright" title="ODbL">open license</a>. Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">',
+    });
+}
 
-const createMarker = (d) => L.marker(d.co).bindPopup(d.na);
-const addLayerToMap = layer => map.addLayer(layer);
+function getOpenStreetMap() {
+    return L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    });
+}
+
+function getGoogleMap() {
+    return new L.Google('ROADMAP');
+}
+
+function createMaps(response) {
+    const maps = {
+        'MapQuest': getMapQuest().addTo(map()),
+        'OpenStreetMap': getOpenStreetMap(),
+        'Google Maps': getGoogleMap(),
+    };
+
+    return [response, maps];
+}
+
+const createMarker = orgUnit => L.marker(orgUnit.co).bindPopup(orgUnit.na);
+const addLayerToMap = layer => {
+    map().addLayer(layer);
+    return layer;
+};
 
 function flipCoordinates(data) {
     data.co.reverse();
@@ -21,14 +48,27 @@ function flipCoordinates(data) {
 }
 
 function createClusterGroupFromMarkers(clusterGroup, marker) {
-    clusterGroup[0].addLayer(marker);
+    clusterGroup.addLayer(marker);
     return clusterGroup;
 }
 
 function addCluster(data) {
-    data
+    return data
         .map(flipCoordinates)
         .map(createMarker)
-        .reduce(createClusterGroupFromMarkers, [new L.MarkerClusterGroup()])
-        .forEach(addLayerToMap);
+        .reduce(createClusterGroupFromMarkers, new L.MarkerClusterGroup());
 }
+
+fetch('/data/sierraleone.json')
+    .then(response => response.json())
+    .then(createMaps)
+    .then(([response, maps]) => {
+        const clusterLayer = addCluster(response);
+
+        L.control
+            .layers({...maps}, {Clusters: clusterLayer})
+            .addTo(map());
+
+        addLayerToMap(clusterLayer);
+    })
+    .catch(ex => console.log('parsing failed', ex));
