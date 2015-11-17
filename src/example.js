cd55c85b0b3f56af39a53129438f36fc27d0df4e
Bjorn Sandvik <bjorn.sandvik@gmail.com>
2015-11-17 18:02:59 +0100

    Temp fix for Leaflet markercluster

--- src/example.js
+++ src/example.js
@@ -5,8 +5,6 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-//var markers = new L.MarkerClusterGroup();
-
 // http://mts.io/2015/04/08/webpack-shims-polyfills/
 fetch('/data/sierraleone.json')
     .then(function(response) {
@@ -16,27 +14,11 @@ fetch('/data/sierraleone.json')
     });
 
 function addCluster(data) {
-    console.log(data);
+    var markers = new L.MarkerClusterGroup();
 
-    //
-    /*
     for (var i = 0; i < data.length; i++) {
         var d = data[i];
         markers.addLayer(L.marker(d.co.reverse()).bindPopup(d.na));
     }
     map.addLayer(markers);
-    */
 }
-
-
-
-/*
- var markers = new L.MarkerClusterGroup();
- $.getJSON('../data/sierraleone.json', function(data){
- for (var i = 0; i < data.length; i++) {
- var d = data[i];
- markers.addLayer(L.marker(d.co.reverse()).bindPopup(d.na));
- }
- });
- map.addLayer(markers);
- */
