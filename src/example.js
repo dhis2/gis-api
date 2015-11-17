a8c7ec9a9a8b441bd35e882f5934337f47ac4ad8
Bjorn Sandvik <bjorn.sandvik@gmail.com>
2015-11-17 18:49:38 +0100

    Added Google basemap

--- src/example.js
+++ src/example.js
@@ -4,20 +4,22 @@ L.Icon.Default.imagePath = 'images';
 
 let map = L.map('map').setView([8.577168, -11.871005], 8);
 
-let mapquest = L.tileLayer("http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png", {
-    subdomains: "1234",
-    attribution: "&copy; <a href='http://www.openstreetmap.org/'>OpenStreetMap</a> and contributors, under an <a href='http://www.openstreetmap.org/copyright' title='ODbL'>open license</a>. Tiles Courtesy of <a href='http://www.mapquest.com/'>MapQuest</a> <img src='http://developer.mapquest.com/content/osm/mq_logo.png'>"
+let mapquest = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+    subdomains: '1234',
+    attribution: '&copy; <a href="http://www.openstreetmap.org/">OpenStreetMap</a> and contributors, under an <a href="http://www.openstreetmap.org/copyright" title="ODbL">open license</a>. Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">',
 }).addTo(map);
 
 let osm = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 });
 
+let google = new L.Google('ROADMAP');
+
 let layerControl = L.control.layers({
     'MapQuest': mapquest,
-    'OpenStreetMap': osm
-}, {}).addTo(map);
-
+    'OpenStreetMap': osm,
+    'Google Maps': google,
+}).addTo(map);
 
 // http://mts.io/2015/04/08/webpack-shims-polyfills/
 fetch('/data/sierraleone.json')
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import L from 'leaflet' ;
 //import 'leaflet.markercluster';
 import '../temp/leaflet.markercluster-fix'; // TODO: Remove when cluster repo compatible with Leaflet 1.0
-
+import '../temp/Google';
 
 
 export default L;
