9cca54690bea46ceda68c1ead3edf61afdbb3da5
Bjorn Sandvik <bjorn.sandvik@gmail.com>
2015-11-26 15:46:16 +0100

    Proportional symbols popup

--- example/layers/layers.js
+++ example/layers/layers.js
@@ -87,6 +87,9 @@ map('map', {
         visible: false,
         features: '/data/facilities-features.json',
         data: '/data/facilities-data.json',
+        popup(feature) {
+            return 'Popup content for ' + feature.properties.na;
+        },
     },
     ],
     bounds: [[6.9679, -13.29096], [9.9432, -10.4887]],
