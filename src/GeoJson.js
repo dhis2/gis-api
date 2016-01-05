4c9728f2d506594e76dbc229af25ac659535b841
Bjorn Sandvik <bjorn@mastermaps.com>
2016-01-05 11:37:48 +0100

    Context menu support

--- src/GeoJson.js
+++ src/GeoJson.js
@@ -3,11 +3,72 @@ import '../temp/leaflet.label-src';
 
 export const GeoJSON = L.GeoJSON.extend({
 
+    options: {
+        highlightStyle: {
+            weight: 2,
+        },
+    },
+
+    addLayer(layer) {
+        const options = this.options;
+        const feature = layer.feature;
+
+        if (options.label) {
+            layer.bindLabel(L.Util.template(options.label, feature.properties));
+        }
+
+        if (options.popup) {
+            layer.bindPopup(L.Util.template(options.popup, feature.properties));
+        }
+
+        L.GeoJSON.prototype.addLayer.call(this, layer);
+    },
+
     setOpacity(opacity) {
         this.setStyle({
             opacity: opacity,
             fillOpacity: opacity,
         });
+    },
+
+    onAdd(map) {
+        L.GeoJSON.prototype.onAdd.call(this, map);
+
+        if (this.options.highlightStyle) {
+            this.on('mouseover', this.onMouseOver, this);
+            this.on('mouseout', this.onMouseOut, this);
+        }
+
+        if (this.options.contextmenu) {
+            this.on('contextmenu', this.options.contextmenu);
+        }
+    },
+
+    onRemove(map) {
+        L.GeoJSON.prototype.onRemove.call(this, map);
+
+        if (this.options.highlightStyle) {
+            this.off('mouseover', this.onMouseOver, this);
+            this.off('mouseout', this.onMouseOut, this);
+        }
+
+        if (this.options.contextmenu) {
+            this.off('contextmenu', this.options.contextmenu);
+        }
+    },
+
+    // Set highlight style
+    onMouseOver(evt) {
+        evt.layer.setStyle(this.options.highlightStyle);
+
+        if (!L.Browser.ie && !L.Browser.opera) {
+            evt.layer.bringToFront();
+        }
+    },
+
+    // Reset style
+    onMouseOut(evt) {
+        this.resetStyle(evt.layer);
     },
 
 });
--- src/Points.js
+++ src/Points.js
@@ -10,75 +10,18 @@ export const Points = GeoJSON.extend({
             fillColor: '#000',
             fillOpacity: 0.7,
         },
-        highlightStyle: {
-            weight: 2,
-        },
-        highlight: true,
     },
 
-    initialize(opts = {}) {
-        const options = L.setOptions(this, opts);
-
+    initialize(options = {}) {
         if (!options.pointToLayer) {
             options.pointToLayer = this.pointToLayer.bind(this);
         }
 
-        this._layers = {};
-
-        if (options.data) {
-            this.addData(options.data);
-        }
-    },
-
-    addLayer(layer) {
-        const options = this.options;
-        const feature = layer.feature;
-
-        if (options.label) {
-            layer.bindLabel(L.Util.template(options.label, feature.properties));
-        }
-
-        if (options.popup) {
-            layer.bindPopup(L.Util.template(options.popup, feature.properties));
-        }
-
-        L.GeoJSON.prototype.addLayer.call(this, layer);
+        GeoJSON.prototype.initialize.call(this, options.data, options);
     },
 
     pointToLayer(feature, latlng) {
         return L.circleMarker(latlng);
-    },
-
-    onAdd(map) {
-        L.GeoJSON.prototype.onAdd.call(this, map);
-
-        if (this.options.highlightStyle) {
-            this.on('mouseover', this.onMouseOver, this);
-            this.on('mouseout', this.onMouseOut, this);
-        }
-    },
-
-    onRemove(map) {
-        L.GeoJSON.prototype.onRemove.call(this, map);
-
-        if (this.options.highlightStyle) {
-            this.off('mouseover', this.onMouseOver, this);
-            this.off('mouseout', this.onMouseOut, this);
-        }
-    },
-
-    // Set highlight style
-    onMouseOver(evt) {
-        evt.layer.setStyle(this.options.highlightStyle);
-
-        if (!L.Browser.ie && !L.Browser.opera) {
-            evt.layer.bringToFront();
-        }
-    },
-
-    // Reset style
-    onMouseOut(evt) {
-        this.resetStyle(evt.layer);
     },
 
 });
