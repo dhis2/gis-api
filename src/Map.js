e6e01be07d3ed95b29782371e531c90731c70f7e
Bjorn Sandvik <bjorn@mastermaps.com>
2016-02-08 15:45:35 +0100

    Measure control

--- src/Map.js
+++ src/Map.js
@@ -14,6 +14,7 @@ import cluster from './Cluster';
 import serverCluster from './ServerCluster';
 import earthEngine from './EarthEngine';
 
+// import '../temp/leaflet.measure/leaflet-measure';
 
 /**
  * Creates a map instance.
