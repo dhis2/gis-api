21fcde7b03ee100977fc0bb3dd09c46180f2ad27
Bjorn Sandvik <bjorn@mastermaps.com>
2016-03-30 10:02:56 +0200

    SSH tiles for MapQuest

--- src/MapQuest.js
+++ src/MapQuest.js
@@ -3,7 +3,7 @@ import L from 'leaflet';
 export const MapQuest = L.TileLayer.extend({
 
     options: {
-        url: 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
+        url: '//otile{s}-s.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
         subdomains: '1234',
         attribution: '&copy; <a href="http://www.mapquest.com/">MapQuest</a>, &copy; <a href="http://www.openstreetmap.org/about">OpenStreetMap</a>',
         maxZoom: 16,
