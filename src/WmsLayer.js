a24deff05433fe4026a5199582e7f9b2ad402610
Bjorn Sandvik <bjorn@mastermaps.com>
2016-08-18 14:14:42 +0200

    WMS options fix

--- src/WmsLayer.js
+++ src/WmsLayer.js
@@ -4,10 +4,11 @@ export const WmsLayer = L.TileLayer.WMS.extend({
 
     options: {
         errorTileUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=',
+        format: 'image/png',
+        transparent: true,
     },
 
-    initialize(opts = {}) {
-        const options = L.setOptions(this, opts);
+    initialize(options) {
         L.TileLayer.WMS.prototype.initialize.call(this, options.url, options);
     },
 
