beb5a49275c2a0401c7ec2fcec9e9d926a226bbf
Bjorn Sandvik <bjorn.sandvik@gmail.com>
2016-11-21 17:43:20 +0100

    New Google Maps plugin, fixes #38

--- src/GoogleLayer.js
+++ src/GoogleLayer.js
@@ -1,50 +1,38 @@
-// Extending https://github.com/shramov/leaflet-plugins/blob/master/layer/tile/Google.js
+// Extending https://gitlab.com/IvanSanchez/Leaflet.GridLayer.GoogleMutant
 // Handles Google API loading
 
 import L from 'leaflet';
-import '../node_modules/leaflet-plugins/layer/tile/Google'; // Extends L above
+import '../node_modules/leaflet.gridlayer.googlemutant/Leaflet.GoogleMutant';
 
-export const GoogleLayer = L.Google.extend({
+export const GoogleLayer = L.GridLayer.GoogleMutant.extend({
 
     options: {
         style: 'ROADMAP', // ROADMAP, SATELLITE, HYBRID, TERRAIN
-        version: '3.24',  // Google Maps API version
+        version: '3.26',  // Google Maps API version
         apiKey: 'AIzaSyBjlDmwuON9lJbPMDlh_LI3zGpGtpK9erc', // Google Maps API key (should be overridden)
-        apiWait: 500, // Milliseconds before checking if Google Maps API is loaded
     },
 
     initialize(opts = {}) {
         const options = L.setOptions(this, opts);
 
-        this._type = options.style;
+        options.type = options.style.toLowerCase();
 
-        if (this.googleMapsApiLoaded()) { // Google Maps API is loaded
-            this._ready = google.maps.Map !== undefined;
-            if (!this._ready) {
-                L.Google.asyncWait.push(this);
-            }
-        } else if (GoogleLayer._mapsApiLoading) { // Google Maps API is currently loading
-            this.waitGoogleMapsApi();
-        } else { // Google Maps API is not loaded
+        // Load Google Maps API if not already loaded/loading
+        if (!this.googleMapsApiLoaded() && !GoogleLayer._mapsApiLoading) {
             this.loadGoogleMapsApi();
         }
+
+        L.GridLayer.GoogleMutant.prototype.initialize.call(this);
     },
 
-    onAdd(map, insertAtTheBottom) {
-        this._map = map;
-        this._insertAtTheBottom = insertAtTheBottom;
-
-        // Add to map if Google Maps API is loaded
-        if (this.googleMapsApiLoaded()) {
-            L.Google.prototype.onAdd.call(this, map, insertAtTheBottom);
-        }
-
+    onAdd(map) {
         L.DomUtil.addClass(map.getContainer(), 'leaflet-google');
+        L.GridLayer.GoogleMutant.prototype.onAdd.call(this, map);
     },
 
     onRemove(map) {
         L.DomUtil.removeClass(map.getContainer(), 'leaflet-google');
-        L.Google.prototype.onRemove.call(this, map);
+        L.GridLayer.GoogleMutant.prototype.onRemove.call(this, map);
     },
 
     // Check if Google Maps API is loaded
@@ -52,36 +40,13 @@ export const GoogleLayer = L.Google.extend({
         return (typeof google !== 'undefined' && typeof google.maps !== 'undefined');
     },
 
-    // Async loading of Google Maps API
+    // Loading of Google Maps API
     loadGoogleMapsApi() {
         GoogleLayer._mapsApiLoading = true;
 
-        // Create random callback function
-        const callbackFunc = 'onGoogleMapsApiReady_' + (Math.random() + 1).toString(36).substring(7);
-
-        // Bind global callback to this instance
-        window[callbackFunc] = this.onGoogleMapsApiLoad.bind(this);
-
         const script = document.createElement('script');
-        script.src = `//maps.googleapis.com/maps/api/js?key=${this.options.apiKey}&v=${this.options.version}&callback=${callbackFunc}`;
+        script.src = `//maps.googleapis.com/maps/api/js?key=${this.options.apiKey}&v=${this.options.version}`;
         document.getElementsByTagName('head')[0].appendChild(script);
-    },
-
-    // Called until Google Maps API is loaded
-    waitGoogleMapsApi() {
-        if (!this.googleMapsApiLoaded()) {
-            setTimeout(this.waitGoogleMapsApi.bind(this), this.options.apiWait);
-            return;
-        }
-        this.onGoogleMapsApiLoad();
-    },
-
-    // Add to map when Google Maps API is loaded
-    onGoogleMapsApiLoad() {
-        this._ready = true;
-        if (this._map) {
-            this.onAdd(this._map, this._insertAtTheBottom);
-        }
     },
 
 });
