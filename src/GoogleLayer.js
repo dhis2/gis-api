07009a86298c5ebf2af294c153cf207be3f64552
Bjørn Sandvik <bjorn@mastermaps.com>
2018-11-19 13:07:04 +0100

    Dependency upgrade and remove package-lock.json

--- src/GoogleLayer.js
+++ src/GoogleLayer.js
@@ -8,7 +8,7 @@ export const GoogleLayer = L.GridLayer.GoogleMutant.extend({
 
     options: {
         style: 'ROADMAP', // ROADMAP, SATELLITE, HYBRID, TERRAIN
-        version: '3.32',  // Google Maps API version
+        version: '3.35',  // Google Maps API version
     },
 
     initialize(opts = {}) {
