4c1c89d18e91d10a9f0eea0fbad8fa13df7c1d6d
Bjørn Sandvik <bjorn@mastermaps.com>
2019-02-14 12:26:04 +0100

    Code cleaning

--- example/choropleth/index.js
+++ example/choropleth/index.js
@@ -1,18 +1,15 @@
-import map from "../../src";
+import map from '../../src';
 
-map("map", {
-  layers: [
-    {
-      type: "mapQuest"
-    },
-    {
-      type: "choropleth",
-      features: "/data/districts-features.json",
-      data: "/data/districts-data.json",
-      popup(feature) {
-        return "Popup content for " + feature.properties.na;
-      }
-    }
-  ],
-  bounds: [[6.9679, -13.29096], [9.9432, -10.4887]]
+map('map', {
+    layers: [{
+        type: 'mapQuest',
+    }, {
+        type: 'choropleth',
+        features: '/data/districts-features.json',
+        data: '/data/districts-data.json',
+        popup(feature) {
+            return 'Popup content for ' + feature.properties.na;
+        },
+    }],
+    bounds: [[6.9679, -13.29096], [9.9432, -10.4887]],
 });
