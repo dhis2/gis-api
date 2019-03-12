238ffeb2abf1fb6bbd3328d29177ce73366fb509
Bjørn Sandvik <bjorn@mastermaps.com>
2019-03-12 16:34:01 +0100

    Only show popup for place search result

--- src/controls/Search.js
+++ src/controls/Search.js
@@ -2,8 +2,24 @@ import L from 'leaflet';
 import 'leaflet-control-geocoder';
 
 // Wrapper for leaflet-control-geocoder: https://github.com/perliedman/leaflet-control-geocoder
-export const Search = L.Control.Geocoder.extend({});
+// Displays a popup for the search result (no marker)
 
 export default function search(options) {
-    return new Search(options);
+    return new L.Control.Geocoder({
+        defaultMarkGeocode: false,
+        ...options,
+    }).on('markgeocode', (evt) => {
+        const map = evt.target._map;
+
+        if (map) {
+            const { bbox, center, html, name } = evt.geocode;
+
+            map.fitBounds(bbox);
+
+            L.popup()
+                .setLatLng(center)
+                .setContent(html || name)
+                .openOn(map);
+        }
+    });
 }
