ea8d605c913de6959dde1076157cff4fd6a9bd80
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-04-02 12:47:23 +0100

    fix: load EarthEngine directly from node_modules browser build

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import Map from './Map';
-import '../scss/gis-api.scss';
+import Map from "./Map";
+import "../scss/gis-api.scss";
 
 // When this file was included in EarthEngine.js it caused error when running tests
 // This file is never loaded by test scripts
-import 'script-loader!./lib/ee_api_js_debug';
+import "script-loader!@google/earthengine/build/ee_api_js";
 
 export default Map;
