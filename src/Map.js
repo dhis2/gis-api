a6824865a6be452b3b6e01274ebc36cfcd49876d
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-04-09 10:24:51 +0200

    chore: install dhis2 style configuration and re-style modified files

--- src/Map.js
+++ src/Map.js
@@ -1,181 +1,184 @@
-import L from "leaflet";
-import layerTypes from "./layerTypes";
-import legend from "./controls/Legend";
-import fitBounds from "./controls/FitBounds";
-import search from "./controls/Search";
-import measure from "./controls/Measure";
+import L from 'leaflet'
+import layerTypes from './layerTypes'
+import legend from './controls/Legend'
+import fitBounds from './controls/FitBounds'
+import search from './controls/Search'
+import measure from './controls/Measure'
 import {
-  toLatLng,
-  toLatLngBounds,
-  toLngLatBounds,
-  getBoundsFromLayers
-} from "./utils/geometry";
+    toLatLng,
+    toLatLngBounds,
+    toLngLatBounds,
+    getBoundsFromLayers,
+} from './utils/geometry'
 
 export class Map extends L.Evented {
-  options = {
-    className: "leaflet-dhis2",
-    zoomControl: false,
-    controls: [],
-    worldCopyJump: true,
-    maxZoom: 18,
-    layerTypes,
-    controlTypes: {
-      legend,
-      fitBounds,
-      search,
-      measure
-    }
-  };
-
-  constructor(el, opts) {
-    super();
-
-    const options = L.setOptions(this, opts);
-
-    this._layers = [];
-
-    this._map = L.map(el, options);
-
-    this._map.attributionControl.setPrefix("");
-
-    L.DomUtil.addClass(this.getContainer(), options.className);
-
-    L.Icon.Default.imagePath = "/images/";
-
-    // Stop propagation to prevent dashboard dragging
-    this._map.on("mousedown", evt => evt.originalEvent.stopPropagation());
-
-    if (options.bounds) {
-      this.fitBounds(options.bounds);
+    options = {
+        className: 'leaflet-dhis2',
+        zoomControl: false,
+        controls: [],
+        worldCopyJump: true,
+        maxZoom: 18,
+        layerTypes,
+        controlTypes: {
+            legend,
+            fitBounds,
+            search,
+            measure,
+        },
     }
 
-    for (const control in options.controls) {
-      if (options.controls.hasOwnProperty(control)) {
-        this.addControl(control);
-      }
+    constructor(el, opts) {
+        super()
+
+        const options = L.setOptions(this, opts)
+
+        this._layers = []
+
+        this._map = L.map(el, options)
+
+        this._map.attributionControl.setPrefix('')
+
+        L.DomUtil.addClass(this.getContainer(), options.className)
+
+        L.Icon.Default.imagePath = '/images/'
+
+        // Stop propagation to prevent dashboard dragging
+        this._map.on('mousedown', evt => evt.originalEvent.stopPropagation())
+
+        if (options.bounds) {
+            this.fitBounds(options.bounds)
+        }
+
+        for (const control in options.controls) {
+            if (options.controls.hasOwnProperty(control)) {
+                this.addControl(control)
+            }
+        }
+
+        this._map.on('click', evt => this.onClick(evt))
+        this._map.on('contextmenu', evt => this.onContextMenu(evt))
     }
 
-    this._map.on("click", evt => this.onClick(evt));
-    this._map.on("contextmenu", evt => this.onContextMenu(evt));
-  }
-
-  getContainer() {
-    return this._map.getContainer();
-  }
-
-  getLeafletMap() {
-    return this._map;
-  }
-
-  // Accept layer as config object
-  addLayer(layer) {
-    const { layerTypes } = this.options;
-    let newLayer = layer;
-
-    if (layer.type && layerTypes[layer.type]) {
-      newLayer = this.createLayer(layer);
+    getContainer() {
+        return this._map.getContainer()
     }
 
-    newLayer.createPane(this._map);
-
-    this._map.addLayer(newLayer);
-    this._layers.push(newLayer);
-
-    return newLayer;
-  }
-
-  removeLayer(layer) {
-    this._layers = this._layers.filter(l => l !== layer);
-    return this._map.removeLayer(layer);
-  }
-
-  hasLayer(layer) {
-    return this._map.hasLayer(layer);
-  }
-
-  remove() {
-    this._map.remove();
-  }
-
-  createLayer(layer) {
-    return this.options.layerTypes[layer.type](layer);
-  }
-
-  addControl(control) {
-    const { position, type } = control;
-    const { controlTypes } = this.options;
-
-    if (position) {
-      control.position = position.replace(/-/, "");
+    getLeafletMap() {
+        return this._map
     }
 
-    let newControl = control;
+    // Accept layer as config object
+    addLayer(layer) {
+        const { layerTypes } = this.options
+        let newLayer = layer
 
-    if (type && controlTypes[type]) {
-      newControl = controlTypes[type](control);
-    } else if (type && L.control[type]) {
-      newControl = L.control[type](control);
+        if (layer.type && layerTypes[layer.type]) {
+            newLayer = this.createLayer(layer)
+        }
+
+        newLayer.createPane(this._map)
+
+        this._map.addLayer(newLayer)
+        this._layers.push(newLayer)
+
+        return newLayer
     }
 
-    this._map.addControl(newControl);
-    return newControl;
-  }
-
-  fitBounds(bounds) {
-    if (bounds) {
-      this._map.fitBounds(toLatLngBounds(bounds));
+    removeLayer(layer) {
+        this._layers = this._layers.filter(l => l !== layer)
+        return this._map.removeLayer(layer)
     }
-  }
 
-  fitWorld() {
-    this._map.fitWorld();
-  }
-
-  setView(lnglat, zoom) {
-    this._map.setView(toLatLng(lnglat), zoom);
-  }
-
-  getLayers() {
-    return this._layers;
-  }
-
-  // Returns the combined bounds for all vector layers
-  getLayersBounds() {
-    return toLngLatBounds(getBoundsFromLayers(this.getLayers()));
-  }
-
-  // Returns true if the layer type is supported
-  hasLayerSupport(type) {
-    return !!this.options.layerTypes[type];
-  }
-
-  openPopup(content, lnglat, onClose) {
-    this._map.openPopup(content, toLatLng(lnglat));
-
-    if (typeof onClose === "function") {
-      this._map.once("popupclose", onClose);
+    hasLayer(layer) {
+        return this._map.hasLayer(layer)
     }
-  }
 
-  resize() {
-    this._map.invalidateSize();
-  }
+    remove() {
+        this._map.remove()
+    }
 
-  onClick(evt) {
-    this.fire("click", this._createClickEvent(evt));
-  }
+    createLayer(layer) {
+        return this.options.layerTypes[layer.type](layer)
+    }
 
-  onContextMenu(evt) {
-    this.fire("contextmenu", this._createClickEvent(evt));
-  }
+    addControl(control) {
+        const { position, type } = control
+        const { controlTypes } = this.options
 
-  _createClickEvent(evt) {
-    const { type, latlng, originalEvent } = evt;
-    const coordinates = [latlng.lng, latlng.lat];
-    const position = [originalEvent.x, originalEvent.pageY || originalEvent.y];
+        if (position) {
+            control.position = position.replace(/-/, '')
+        }
 
-    return { type, coordinates, position };
-  }
+        let newControl = control
+
+        if (type && controlTypes[type]) {
+            newControl = controlTypes[type](control)
+        } else if (type && L.control[type]) {
+            newControl = L.control[type](control)
+        }
+
+        this._map.addControl(newControl)
+        return newControl
+    }
+
+    fitBounds(bounds) {
+        if (bounds) {
+            this._map.fitBounds(toLatLngBounds(bounds))
+        }
+    }
+
+    fitWorld() {
+        this._map.fitWorld()
+    }
+
+    setView(lnglat, zoom) {
+        this._map.setView(toLatLng(lnglat), zoom)
+    }
+
+    getLayers() {
+        return this._layers
+    }
+
+    // Returns the combined bounds for all vector layers
+    getLayersBounds() {
+        return toLngLatBounds(getBoundsFromLayers(this.getLayers()))
+    }
+
+    // Returns true if the layer type is supported
+    hasLayerSupport(type) {
+        return !!this.options.layerTypes[type]
+    }
+
+    openPopup(content, lnglat, onClose) {
+        this._map.openPopup(content, toLatLng(lnglat))
+
+        if (typeof onClose === 'function') {
+            this._map.once('popupclose', onClose)
+        }
+    }
+
+    resize() {
+        this._map.invalidateSize()
+    }
+
+    onClick(evt) {
+        this.fire('click', this._createClickEvent(evt))
+    }
+
+    onContextMenu(evt) {
+        this.fire('contextmenu', this._createClickEvent(evt))
+    }
+
+    _createClickEvent(evt) {
+        const { type, latlng, originalEvent } = evt
+        const coordinates = [latlng.lng, latlng.lat]
+        const position = [
+            originalEvent.x,
+            originalEvent.pageY || originalEvent.y,
+        ]
+
+        return { type, coordinates, position }
+    }
 }
 
-export default Map;
+export default Map
