cd7f7db47dba5eafe38c306060b0c519ce4aa226
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-04-17 22:37:18 +0200

    fix: override circle getBounds before map is bound

--- src/layers/Buffer.js
+++ src/layers/Buffer.js
@@ -1,6 +1,7 @@
-import L from 'leaflet';
-import turfBuffer from '@turf/buffer';
-import { GeoJson } from './GeoJson';
+import L from 'leaflet'
+import turfBuffer from '@turf/buffer'
+import { GeoJson } from './GeoJson'
+import { Circle } from './Circle'
 
 export const Buffer = GeoJson.extend({
     options: {
@@ -17,46 +18,48 @@ export const Buffer = GeoJson.extend({
 
     initialize(options = {}) {
         // Replace polygons with buffered geometries
-        options.data = options.data.map(feature => 
-            feature.geometry.type !== 'Point' ? {
-                ...feature,
-                geometry: turfBuffer(feature, options.buffer / 1000).geometry, // km
-            } : feature
-        );
+        options.data = options.data.map(feature =>
+            feature.geometry.type !== 'Point'
+                ? {
+                      ...feature,
+                      geometry: turfBuffer(feature, options.buffer / 1000)
+                          .geometry, // km
+                  }
+                : feature
+        )
 
-        GeoJson.prototype.initialize.call(this, options);
+        GeoJson.prototype.initialize.call(this, options)
     },
 
     // Set color from feature
     addLayer(layer) {
-        const { feature } = layer;
-        const { color } = feature.properties;
+        const { feature } = layer
+        const { color } = feature.properties
 
         if (color && layer.setStyle) {
-            layer.setStyle({ color });
+            layer.setStyle({ color })
         }
 
-        GeoJson.prototype.addLayer.call(this, layer);
+        GeoJson.prototype.addLayer.call(this, layer)
     },
 
     pointToLayer(geojson, latlng) {
-        this.options.style.pane = this.options.pane;
-
         // Draw points as circles using buffer as radius
-        return L.circle(latlng, {
+        return new Circle(latlng, {
             ...this.options.style,
+            pane: this.options.pane,
             radius: this.options.buffer,
-        });
+        })
     },
 
     setOpacity(opacity) {
         this.setStyle({
             opacity,
             fillOpacity: opacity * this.options.opacityFactor,
-        });
+        })
     },
-});
+})
 
 export default function buffer(options) {
-    return new Buffer(options);
+    return new Buffer(options)
 }
