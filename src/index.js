6bde95c2d48067429f328ee48cb585157c955a9c
Bjorn Sandvik <bjorn@dhis2.org>
2020-01-16 13:30:35 +0100

    feat: export supported layer types (#73)
    * feat: export supported layer types
    
    * Code clean

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import Map from "./Map";
+import types from './layerTypes';
 import "../scss/gis-api.scss";
 
 // When this file was included in EarthEngine.js it caused error when running tests
 // This file is never loaded by test scripts
 import "script-loader!@google/earthengine/build/ee_api_js";
+
+// Export supported layer types as an array
+export const layerTypes = Object.keys(types);
 
 export default Map;
